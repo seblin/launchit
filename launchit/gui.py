2fb9ece8d0ef50f1dda2fda830ff5736d1a9b5fa
Sebastian Linke <seblin@arcor.de>
2011-03-02 14:35:14 +0100

    Introduced gui.MarkedCompletionDelegate(), which is now used to render marked fragments on a completion.

--- launchit/gui.py
+++ launchit/gui.py
@@ -5,6 +5,28 @@ from PySide import QtGui
 # launchit package
 from . import core
 
+class MarkedCompletionDelegate(QtGui.QItemDelegate):
+    def __init__(self, start_mark='<b><u>', end_mark='</u></b>', parent=None):
+        QtGui.QItemDelegate.__init__(self, parent)
+        self.start_mark = start_mark
+        self.end_mark = end_mark
+        self.fragment = ''
+        self.renderer = QtGui.QTextDocument()
+
+    def paint(self, painter, option, index):
+        self.drawBackground(painter, option, index)
+
+        painter.save()
+        painter.translate(option.rect.topLeft())
+        markup = core.get_marked_completion(index.data(), self.fragment,
+                                            self.start_mark, self.end_mark)
+        self.renderer.setHtml(markup)
+        self.renderer.drawContents(painter)
+        painter.restore()
+
+    def sizeHint(self, option, index):
+        return self.renderer.size().toSize()
+
 class CommandlineCompleter(QtGui.QCompleter):
     def __init__(self, parent=None):
         QtGui.QCompleter.__init__(self, parent)
@@ -12,10 +34,13 @@ class CommandlineCompleter(QtGui.QCompleter):
         self.setCompletionMode(mode)
         model = QtGui.QStringListModel()
         self.setModel(model)
+        delegate = MarkedCompletionDelegate()
+        self.popup().setItemDelegate(delegate)
 
     def update(self, fragment):
         completions = core.get_name_completions(fragment)
         self.model().setStringList(completions)
+        self.popup().itemDelegate().fragment = fragment
 
 class LaunchEdit(QtGui.QLineEdit):
     def __init__(self, parent=None):
