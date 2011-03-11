573dbb325408d2d8bad7f23ea7fbe52b6a3b44d1
Sebastian Linke <seblin@arcor.de>
2011-03-11 13:45:47 +0100

    Added special handling for oversized completion names, which do not fit into the popup.

--- launchit/gui.py
+++ launchit/gui.py
@@ -16,14 +16,33 @@ class MarkedCompletionDelegate(QtGui.QItemDelegate):
 
     def paint(self, painter, option, index):
         self.drawBackground(painter, option, index)
+        self.draw_contents(painter, option, index)
 
+    def draw_contents(self, painter, option, index):
         painter.save()
         painter.translate(option.rect.topLeft())
-        markup = core.get_marked_completion(index.data(), self.fragment,
-                                            self.start_mark, self.end_mark)
-        self.renderer.setHtml(markup)
-        self.renderer.drawContents(painter)
+        self._draw_marked_text(painter, index.data(), option.rect.width())
         painter.restore()
+
+    def _draw_marked_text(self, painter, text, width):
+        # Note that truncation can't be made with QFontMetrics.elidedText() 
+        # here, since that doesn't handle rich text. The trial and error 
+        # approach has been chosen in order to avoid messing with the 
+        # rendering engine. There probably should be no situation where 
+        # memory consumption of this method is really getting noticeable.
+        ellipsis = '...'
+        rendered_width = self.renderer.idealWidth
+        markup = self._markup_for_completion(text)
+        self.renderer.setHtml(markup)
+        while text and (rendered_width() > width):
+            text = text[:-1]
+            markup = self._markup_for_completion(text)
+            self.renderer.setHtml(markup + ellipsis)
+        self.renderer.drawContents(painter)
+
+    def _markup_for_completion(self, completion):
+        return core.get_marked_completion(completion, self.fragment, 
+                                          self.start_mark, self.end_mark)
 
     def sizeHint(self, option, index):
         return self.renderer.size().toSize()
