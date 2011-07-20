f5bd57820645c573242b2f318ab9a623c6df2fff
seblin <seblin@arcor.de>
2011-07-20 07:07:34 +0200

    Added metainfo for author, license and version to `__init__.py`.

--- launchit/__init__.py
+++ launchit/__init__.py
@@ -9,4 +9,8 @@ Some features:
 - Launch files with their associated application (images, scripts, ...)
 - A GUI to conveniently benefit of those features
 """
+__author__ = 'Sebastian Linke'
+__license__ = 'MIT'
+__version__ = '0.1-dev'
+
 from . import core, gui, icongetter, settings
