e19a578024aa64be8b8762b85e3564eed30afa65
seblin <seblin@arcor.de>
2011-07-20 06:51:55 +0200

    Created docstring inside `__init__.py` to explain launchit's main features.

--- launchit/__init__.py
+++ launchit/__init__.py
@@ -1,1 +1,12 @@
+"""
+Tools to launch files and commands.
+
+Some features:
+
+- Make name completions based on given fragment
+- Mark fragment inside a completion
+- Guess suitable icon when dealing with a command or file
+- Launch files with their associated application (images, scripts, ...)
+- A GUI to conveniently benefit of those features
+"""
 from . import core, gui, icongetter, settings
