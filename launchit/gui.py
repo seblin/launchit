35002617470b2516a6729bd1a19b311cb09350b8
seblin <seblin@arcor.de>
2011-08-11 00:03:17 +0200

    Introduced `gui.CompletionMarkupBuilder()` to separate markup building from the delegate.

--- launchit/gui.py
+++ launchit/gui.py
@@ -8,46 +8,57 @@ from PySide import QtGui
 # launchit package
 from . import core, icongetter, settings
 
-class MarkedCompletionDelegate(QtGui.QItemDelegate):
-    def __init__(self, start_mark='<b><u>', end_mark='</u></b>', parent=None):
-        QtGui.QItemDelegate.__init__(self, parent)
+class CompletionMarkupBuilder(object):
+    def __init__(self, start_mark='<b><u>', end_mark='</u></b>'):
         self.start_mark = start_mark
         self.end_mark = end_mark
+
+    def get_elided_markup(self, text, fragment, width):
+        # Note that truncation can't be made with QFontMetrics.elidedText()
+        # here, since that doesn't handle rich text. The trial and error
+        # approach has been chosen in order to avoid messing with the
+        # rendering engine. There probably should be no situation where
+        # memory consumption of this method is really getting noticeable.
+        markup = self.get_completion_markup(text, fragment)
+        temporary_renderer = QtGui.QTextDocument()
+        temporary_renderer.setHtml(markup)
+        rendered_width = temporary_renderer.idealWidth
+        ellipsis = '...'
+        while text and (rendered_width() > width):
+            text = text[:-1]
+            markup = self.get_completion_markup(text, fragment) + ellipsis
+            temporary_renderer.setHtml(markup)
+        return markup
+
+    def get_completion_markup(self, completion, fragment):
+        return core.get_marked_completion(completion, fragment,
+                                          self.start_mark, self.end_mark)
+
+class MarkedCompletionDelegate(QtGui.QItemDelegate):
+    def __init__(self, markup_builder=CompletionMarkupBuilder(), parent=None):
+        QtGui.QItemDelegate.__init__(self, parent)
+        self.markup_builder = markup_builder
+        self._renderer = QtGui.QTextDocument()
         self.fragment = ''
-        self.renderer = QtGui.QTextDocument()
 
     def paint(self, painter, option, index):
         self.drawBackground(painter, option, index)
         self.draw_contents(painter, option, index)
 
     def draw_contents(self, painter, option, index):
+        markup = self.markup_builder.get_elided_markup(
+            index.data(), self.fragment, option.rect.width())
+        self.draw_markup(markup, painter, option.rect.topLeft())
+
+    def draw_markup(self, markup, painter, start_pos):
+        self._renderer.setHtml(markup)
         painter.save()
-        painter.translate(option.rect.topLeft())
-        self._draw_marked_text(painter, index.data(), option.rect.width())
+        painter.translate(start_pos)
+        self._renderer.drawContents(painter)
         painter.restore()
 
-    def _draw_marked_text(self, painter, text, width):
-        # Note that truncation can't be made with QFontMetrics.elidedText()
-        # here, since that doesn't handle rich text. The trial and error
-        # approach has been chosen in order to avoid messing with the
-        # rendering engine. There probably should be no situation where
-        # memory consumption of this method is really getting noticeable.
-        ellipsis = '...'
-        rendered_width = self.renderer.idealWidth
-        markup = self._markup_for_completion(text)
-        self.renderer.setHtml(markup)
-        while text and (rendered_width() > width):
-            text = text[:-1]
-            markup = self._markup_for_completion(text)
-            self.renderer.setHtml(markup + ellipsis)
-        self.renderer.drawContents(painter)
-
-    def _markup_for_completion(self, completion):
-        return core.get_marked_completion(completion, self.fragment,
-                                          self.start_mark, self.end_mark)
-
     def sizeHint(self, option, index):
-        return self.renderer.size().toSize()
+        return self._renderer.size().toSize()
 
 class CommandlineCompleter(QtGui.QCompleter):
     def __init__(self, parent=None):
